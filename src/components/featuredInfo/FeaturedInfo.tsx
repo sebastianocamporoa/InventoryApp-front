40beec171ab0eb0ead2aca193e1ef75e9a2ff73b
Sebastian Ocampo Roa <seocampo@personalsoft.com>
2023-10-20 12:23:29 -0500

    add resume data

--- src/components/featuredInfo/FeaturedInfo.tsx
+++ src/components/featuredInfo/FeaturedInfo.tsx
@@ -1,26 +1,50 @@
+import { useEffect, useState } from "react";
 import "./featuredInfo.scss";
+import api from "../../api";
 
 const FeaturedInfo = () => {
+  type DashboardSummary = {
+    totalProducts: number;
+    totalSales: number;
+    totalEarnings: number;
+  };
+  const [data, setData] = useState<DashboardSummary | null>(null);
+
+  const getResume = async () => {
+    await api
+      .get("/dashboard/summary")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getResume();
+  }, []);
+
   return (
     <div className="featuredInfoComponent">
       <div className="item">
         <span className="title">Total productos</span>
         <div>
-          <span className="money">0</span>
+          <span className="money">{data?.totalProducts || 0}</span>
         </div>
       </div>
 
       <div className="item">
         <span className="title">Total ventas</span>
         <div>
-          <span className="money">0</span>
+          <span className="money">{data?.totalSales || 0}</span>
         </div>
       </div>
 
       <div className="item">
         <span className="title">Total ganancias por ventas</span>
         <div>
-          <span className="money">$0</span>
+          <span className="money">${data?.totalEarnings || 0}</span>
         </div>
       </div>
     </div>
